445e6363994c9a2c63e51d4f01a13ef252580ea6
Kent Martin Åsvang <github@asvang.no>
2023-04-28 15:19:56 +0200

    updated to .NET 7.0 and added small test-project

---
+++ src/Kent.PdfLatex.ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Kent.PdfLatex;
+
+Console.WriteLine("Hello, World!");
+
+var outputDirectory = "../../../../../external/Output";
+var filePath = "../../../../../external/Templates/test.tex";
+var templateDirectory = "../../../../../external/Templates";
+
+var pdfLatexBuilder = new PdfLatexBuilder()
+    .OutputDirectory(outputDirectory)
+    .File(filePath)
+    .EnableInstaller()
+    .IncludeDirectory(templateDirectory)
+    .NonStopMode();
+    
+await pdfLatexBuilder.Run();
--- src/Kent.PdfLatex/PdfLatexBuilder.cs
+++ src/Kent.PdfLatex/PdfLatexBuilder.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Text;
 
-namespace kentaasvang.PdfLatex;
+namespace Kent.PdfLatex;
 
 public class PdfLatexBuilder
 {
